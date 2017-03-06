cfd3ce8681c20649c1c9a4d0ef4fe521e926f37a
fergmac <f.macconnell@gmail.com>
2017-03-05 20:47:37 -0800

    refactor app to work with local storage

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -4,47 +4,11 @@ var moment =require('moment');
 
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
-var TodoSearch = require('TodoSearch');
-var TodoAPI = require('TodoAPI');
+import TodoSearch from 'TodoSearch';
+
 
 var TodoApp = React.createClass({
-    getInitialState: function () {
-        return {
-            showCompleted: false,
-            //return all todos no mater what the text is
-            searchText: '',
-            //when app starts we get todos
-            todos: TodoAPI.getTodos()
-        }
-    },
-    componentDidUpdate: function () {
-        //when app updates we set the todos
-        TodoAPI.setTodos(this.state.todos);
-    },
-    handleAddTodo: function (text) {
-        //using spread we set new todos array equal to old one with new object attached to the end, using uuid node package for radom id's
-        this.setState({
-            todos: [
-                ...this.state.todos,
-                {
-                    id: uuid(),
-                    text: text,
-                    completed: false,
-                    createdAt: moment().unix(),
-                    completedAt: undefined
-                }
-            ]
-        });
-    },
-    handleSearch: function (showCompleted, searchText) {
-        this.setState({
-            showCompleted: showCompleted,
-            searchText: searchText.toLowerCase()
-        });
-    },
     render: function ()  {
-        var {todos, showCompleted, searchText} = this.state;
-        var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
         return (
             <div>
                 <h1 className="page-title">Todo App</h1>
@@ -52,9 +16,9 @@ var TodoApp = React.createClass({
                 <div className="row">
                     <div className="column small-centered small-11 medium-6 large-5">
                         <div className="container">
-                            <TodoSearch onSearch={this.handleSearch}/>
+                            <TodoSearch />
                             <TodoList />
-                            <AddTodo onAddTodo={this.handleAddTodo}/>
+                            <AddTodo />
                         </div>
                     </div>
                 </div>
