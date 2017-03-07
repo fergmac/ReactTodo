700a037e60c34ba9a18c4cc14574450d389a3cda
fergmac <f.macconnell@gmail.com>
2017-03-06 20:45:50 -0800

    update todos on firebase

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -1,27 +1,6 @@
 var $ =require('jquery');
 
 module.exports = {
-    setTodos: function (todos) {
-        if($.isArray(todos)) {
-            //takes two strings so we turn array into string
-            localStorage.setItem('todos', JSON.stringify(todos));
-            return todos;
-        }
-    },
-    getTodos: function () {
-        //takes one string
-        var stringTodos = localStorage.getItem('todos');
-        var todos = [];
-
-        // catch fires if error inside of try
-        try {
-            todos = JSON.parse(stringTodos);
-        } catch (e) {
-
-        }
-
-        return $.isArray(todos) ? todos : [];
-    },
     filterTodos: function (todos, showCompleted, searchText) {
         var filteredTodos = todos;
 
